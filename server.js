8a315fd9301f83e18632c2aaad78dda75c67556a
gillchristian <gillchristiang@gmail.com>
2015-08-23 01:57:07 -0300

    server.js
    started working on the API with a simplified model

--- server.js
+++ server.js
@@ -8,233 +8,116 @@ var express 	= require('express');
 var app			= express(),
 	bodyParser 	= require('body-parser'),
 	mongoose	= require('mongoose'),
-	config		= requiere('./config');
+	config		= require('./config');
 
 
 // conect to the database
 mongoose.connect(config.database);
 
-app.use(bodyParser.json()); // support json encode bodies
-app.use(bodyParser.urlencoded({extend: true})); // support encoded bodies
+app.use( bodyParser.json() ); // support json encode bodies
+app.use( bodyParser.urlencoded({extend: true}) ); // support encoded bodies
 
 var User = require('./models/user.js');
 
 // =========================================================================
-// ===== mongoDB ===========================================================
-// =========================================================================
-
-var chris = new User ({
-	name: 'Chris',
-	mail: 'chrisdude@gmail.com',
-	lastname: 'Dude',
-	phone: '1234567890'
-});
-
-chris.save(function(err){
-	if(err) throw err;
-
-	console.log('User saved!');
-});
-
-// get all the users
-User.find({}, function(err, users){
-	if (err) throw err;
-
-	console.log(users);
-});
-
-// get the user chris
-User.find({name: 'chris'}, function(err, user){
-	if (err) throw err;
-
-	console.log(user);
-});
-
-// find by ID: id of 1
-/*
-User.findById(1, function(err, user){
-	if (err) throw err;
-
-	console.log(user);
-});
-
-// get any admin that was created in the past month
-
-// get the date 1 month ago
-var monthAgo = new Date();
-monthAgo.setMonth(monthAgo.getMonth() - 1);
-
-User.find({ admin: true }).where('created_at').gt(monthAgo).exec(function(err, users) {
-  if (err) throw err;
-
-  // show the admins in the past month
-  console.log(users);
-});
-
-// get user with ID 1 then update
-User.findById(1, function(err, user){
-	if (err) throw err;
-
-	user.email += '.ar';
-
-	user.save(function(err){
-		if (err) throw err;
-
-			console.log('User update!!');
-	});
-});
-
-// finde by ID and update
-User.findByIdAndUpdate(1, {username: 'rand user'}, function(err, user){
-	if (err) throw err;
-
-	console.log(user);
-});
-*/
-
-// find and update
-
-User.findOneAndUpdate({name: 'chris'}, {name: 'chris the dude'}, function(err, ser){
-	if (err) throw err;
-
-	console.log(user);
-});
-
-
-// get usern then remove
-User.find({name: 'chris'}, function(err, user){
-	if (err) throw err;
-
-	user.remove(function(err){
-		if (err) throw err;
-
-		console.log('User removed :/');
-	});
-});
-
-// finde and remove
-User.findOneAndRemove({name: 'chris'}, function(err){
-	if (err) throw err;
-
-	console.log('User removed :/');
-});
-
-/*
-// find by ID and remove
-User.findByIdAndRemove(4, function(err){
-	if (err) throw err;
-
-	console.log('User removed :/');
-});
-*/
-
-var ro = new User ({
-	name: 'Ro',
-	mail: 'rothelady@gmail.com',
-	lastname: 'Lady',
-	phone: '1234567890'
-});
-
-ro.save(function(err){
-	if(err) throw err;
-
-	console.log('User saved!');
-});
-// =========================================================================
 // ===== Routes ============================================================
 // =========================================================================
 
-app.route('/login')
-	
-	// show the form (GET hhtp://localhost:8080/login)
-	.get(function(req, res) {
-		res.send('this is the login form!');
-	})
+// get an instance of the Express Router
 
-	// process the form (POST htto://localhost:8080/login)
-	.post(function(req, res){
-		console.log('processing the form');
-		res.send('processing the login form!');
-	});
-
-
-// sample route
-app.get('/sample', function(req, res) {
-
-	res.send('this is a sample!');
-});
-
-// Specific Routing for Specific Parameters --- (GET)
-app.get('/sample/check/:version', function(req, res) {
-
-	res.send('version: ' + req.params.version);
-});
-
-// Getting Any URL Parameter
-app.get('/sample/user', function(req, res) {
-  var user_id = req.param('id');
-  var token = req.param('token');
-  var geo = req.param('geo');  
-
-  res.send(user_id + ' ' + token + ' ' + geo);
-});
-
-// POST Parameters
-
-app.post('/sample/users', function(req, res){
-	var user_id = req.body.id;
-	var token = req.body.token;
-	var geo = req.body.geo;
-
-	res.send(user_id + ' ' + token + ' ' + geo);
-})
-
-// routes here ------------------ 
-
-// instance of router
 var router = express.Router();
 
-// middleware that happens on every request
+// middleware that happens on every request ---- log requests
 router.use(function(req, res, next){
 
-	// log each request
 	console.log(req.method, req.url);
-
-	// continue
+	// continue what we are doing
 	next();
 });
 
-// home page route (http://localhost:8080)
-router.get ('/', function(req, res){
-	res.send('this is the home page :)');
+// basic api route :)
+
+router.get('/', function(req, res){
+	res.send('Welcome to the Volunteers Center API :)');
 });
 
-// about page route (http://localhost:8080/about)
-router.get('/about', function(req, res){
 
-	res.send('about page :) ');
-});
+// routes --- localhost:8080/users
+router.route('/users')
 
-// route middleware to validate :name
-router.param('name', function(req, res, next, name){
+	.get(function(req, res){
+		
+		// find all \o/ the users
+		User.find({}, function(err, users){
+			
+			if (err) throw err;
+			console.log(users);
+			console.log('------------------------');
+			// send the users 
+			res.json(users);
+		});
+	})
 
-	// here goes the validation :)
-	console.log(name + ' working hard on that validation ;)');
+	.post(function(req, res){
 
-	// after validation save the new item
-	req.name = name + ' capologies';
+		var user 	= new User();
 
-	// continue
-	next();
-});
+		user.name 		= req.body.name,
+		user.lastname 	= req.body.lastname,
+		user.email 		= req.body.email,
+		user.phone 		= req.body.phone,
 
-// route with parameters (http://localhost:8080/hello/:name)
-router.get('/hello/:name', function(req, res) {
-	res.send('hello ' + req.name + '!');
-});
+		user.save(function(err){
+			if(err) throw err;
+
+			console.log('User saved!');
+		});
+
+	});
+
+// routes --- localhost:8080/users/:user_id
+router.route('/users/:_id')
+	
+	.get(function(req, res){
+
+		User.findById(req.param._id, function(err, user){
+			if (err) throw err;
+
+			console.log(user);
+			console.log('------------------------');
+			res.json(user);
+		});
+	})
+
+	.put(function(req, res){
+
+		User.findById(req.param._id, function(err, user){
+			
+			if (err) throw err;
+
+			if (req.body.name) user.name = req.body.name;
+			if (req.body.lastname) user.lastname = req.body.lastname;
+			if (req.body.phone) user.phone = req.body.phone;
+			if (req.body.email) user.email = req.body.email;
+
+			user.save(function(err){
+				if (err) throw err;
+
+				res.send('User Updated!!!');
+				console.log('User Updated!!!');
+			});
+		});
+	})
+
+	.delete(function(req, res){
+		User.remove({_id: req.param._id }, function(err, user){
+			if (err) throw err;
+			res.send('Successfully deleted');
+		});
+	});
 
 // applay the routes to our app
-app.use('/', router);
+app.use('/api', router);
 
 // =========================================================================
 // ===== Start the server ==================================================
